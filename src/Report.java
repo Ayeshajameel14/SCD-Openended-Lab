34cbed37c40ea39cf9f08c8a6f052c64c0803b2b
Ayeshajameel14 <ayeshajameel765@gmail.com>
2025-10-23 18:35:52 +0500

    Updated Report.java file

--- src/Report.java
+++ src/Report.java
@@ -15,6 +15,8 @@ public class Report {
         this.femaleVisitors = femaleVisitors;
         this.totalTicketSold = totalTicketSold;
         this.totalRevenue = totalRevenue;
+
+        
     }
 
     @Override
